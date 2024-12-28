29d74d02fe8169eb2d93437936243b81022de0d0
AntonyMena999 <ast.mena@yavirac.edu.ec>
2024-12-28 00:53:21 -0500

    avance 1

---
+++ src/app/admin/layout/app.menu.component.ts
@@ -0,0 +1,36 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LayoutService } from './service/app.layout.service';
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './app.menu.component.html'
+})
+export class AppMenuComponent implements OnInit {
+
+    model: any[] = [];
+
+    constructor(public layoutService: LayoutService) { }
+
+    ngOnInit() {
+        this.model = [
+            
+            //{
+                //label: 'Administración',
+                //items: [
+                    //{ label: 'Perfil', icon: 'pi pi-fw pi-user', routerLink: ['/admin/perfil'] },
+                    //{ label: 'Cliente', icon: 'pi pi-check', routerLink: ['/admin/cliente'] }
+                //]
+            //},
+            {
+                //label: 'Inventario',
+                items: [
+                    { label: 'Portal para Pacientes', icon: 'pi pi-eye', routerLink: ['/admin/categoria'] },
+                    //{ label: 'Producto', icon: 'pi pi-fw pi-check-square', routerLink: ['/admin/producto'] },
+                    
+                ]
+            },
+                 
+        ];
+    }
+}
